8178ae6fbe9f5889ddc1154f9bb30f3d9f4050e5
sammckenna1986 <sammckenna1986@gmail.com>
2020-09-10 13:04:39 +0000

    put folders and app.py and templates in place

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import dns
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'smells_good'
+app.config["MONGO_URI"] = 'mongodb+srv://sam_mckenna:YnNPIkpu2VIdHiqT@cluster0.wptnw.mongodb.net/smells_good?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/recipes')
+def get_tasks():
+    return render_template("home.html", recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
