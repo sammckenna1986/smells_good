c8ba44206c88728b8ca39954dbfd42d8f95db014
sammckenna1986 <sammckenna1986@gmail.com>
2020-09-21 21:19:41 +0000

    created create account page and worked out password input; Also worked out how to upload an image to the data base for the recipes

--- app.py
+++ app.py
@@ -30,11 +30,43 @@ def create_recipe():
     return render_template("create_recipe.html",
     categories=mongo.db.categories.find(), recipes=mongo.db.categories.find())
 
+
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('create_recipe'))
+
+@app.route('/create_account')
+def create_account():
+    return render_template("create_account.html",
+    users=mongo.db.users.find())
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    users = mongo.db.users
+    users.insert_one(request.form.to_dict())
+    return redirect(url_for('create_recipe'))
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'recipe_image' in request.files:
+        recipe_image = request.files['recipe_image']
+        mongo.save_file(recipe_image.filename, recipe_image)
+    return redirect(url_for('create_recipe'))
+
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+'''
+@app.route('/profile/<username>')
+def profile(username):
+    user = mongo.db.users.find_one_or_404({'username' : username})
+        return render_template("user.html", recipes=mongo.db.users.find())
+'''
+
+
+
 
 @app.route('/recipe')
 def recipe():
